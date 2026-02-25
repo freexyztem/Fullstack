0b1bbf48866788ade7f17aeec7ff11f222b43b7a
Alexi Gonzalez <freexyztem@gmail.com>
2026-02-25 17:18:46 -0500

    comenzando en 4c-1

---
+++ Ejercicios/4-Python/4c-1_datos_cinematograficos.py
@@ -0,0 +1,19 @@
+# importar modulos
+import numpy as np
+
+# datos de peliculas en un array numpy
+peliculas = np.array(
+    [
+        ["Peli 1", "Comedia", 120, 1990, 8.5],
+        ["Peli 2", "Acción", 110, 2005, 7.8],
+        ["Peli 3", "Drama", 95, 2010, 6.9],
+        ["Peli 4", "Comedia", 100, 1985, 7.5],
+        ["Peli 5", "Acción", 130, 2015, 8.1],
+        ["Peli 6", "Drama", 115, 2000, 6.7],
+        ["Peli 7", "Comedia", 90, 1995, 8.2],
+        ["Peli 8", "Acción", 105, 2010, 7.4],
+        ["Peli 9", "Drama", 125, 1980, 6.8],
+        ["Peli 10", "Comedia", 95, 2000, 8.0],
+    ]
+)
+print(peliculas)
